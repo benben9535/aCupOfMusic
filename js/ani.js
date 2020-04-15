25cb16b7978a7fed26d419db209f8a511dcdfa5c
benben9535 <benben9535@gmail.com>
2020-04-15 16:50:37 +0800

    first commit

---
+++ js/ani.js
@@ -0,0 +1,10 @@
+
+
+    window.addEventListener("load",function ani(){
+    TweenMax.staggerTo('.box3', 1, {
+        y:10,
+        opacity: 1,
+        ease: Back.easeOut.config(1.7),
+        }, 0.3);
+    })
+
